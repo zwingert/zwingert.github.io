b018ab597091a9977d8ddbe3fa6583163e7e0010
Zach <zwingert@iastate.edu>
2021-07-27 17:51:25 -0500

    Set up the files and started the backbone of the html

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+const iconElement = document.querySelector(".weather-pic");
+const iconElement = document.querySelector(".temp-degree");
+const iconElement = document.querySelector(".temp-desc");
+const iconElement = document.querySelector(".address");
+
+const weather = {};
+
+weather.temp = {
+    unit : "celsius"
+}
